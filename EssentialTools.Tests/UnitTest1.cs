3034464bda6acb3d87e972287e123db4b3a3e45d
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-04 10:46:03 -0400

    adds another test method

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -22,5 +22,19 @@ namespace EssentialTools.Tests
             // assert
             Assert.AreEqual(total * 0.9M, discountedTotal);
         }
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            // arrange
+            IDiscountHelper target = getTestObject();
+            // act
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+            // assert
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+        }
     }
 }
