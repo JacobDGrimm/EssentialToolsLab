3383ceef81d931e39c29df9f800e317dddf461d4
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-04 08:28:57 -0400

    adds the IValueCalculator interface and subsequently decouples the linqcalculator from the shopping cart

--- EssentialTools/Models/ShoppingCart.cs
+++ EssentialTools/Models/ShoppingCart.cs
@@ -7,8 +7,8 @@ namespace EssentialTools.Models
 {
     public class ShoppingCart
     {
-        private LinqValueCalculator calc;
-        public ShoppingCart(LinqValueCalculator calcParam) { calc = calcParam; }
+        private IValueCalculator calc;
+        public ShoppingCart(IValueCalculator calcParam) { calc = calcParam; }
         public IEnumerable<Product> Products { get; set; }
         public decimal CalculateProductTotal()
         {
